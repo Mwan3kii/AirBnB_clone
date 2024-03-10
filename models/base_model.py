61b62f2d27a5b4aaed9355536e7eea4aec343267
Mwan3kii <agathawanja303@gmail.com>
2024-03-10 20:53:45 +0300

    basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -19,7 +19,7 @@ class BaseModel:
         if kwargs:
             for key, value in kwargs.items():
                 if key == 'created_at' or key == 'updated_at':
-                    setattr(self, key, datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
+                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
                 elif key != '__class__':
                     setattr(self, key, value)
         else:

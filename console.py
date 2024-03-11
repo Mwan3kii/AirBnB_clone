23034457083cd49cd7b384f2a29120d98635585b
Mwan3kii <agathawanja303@gmail.com>
2024-03-11 17:03:30 +0300

    console

--- console.py
+++ console.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
 import cmd
 
@@ -24,25 +24,6 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """Doesnt execute anything when entered"""
         pass
-
-    def classes(self):
-
-        from models.base_model import BaseModel
-        from models.user import User
-        from models.state import State
-        from models.city import City
-        from models.amenity import Amenity
-        from models.place import Place
-        from models.review import Review
-
-        classes = {"BaseModel": BaseModel,
-                "User": User,
-                "State": State,
-                "City": City,
-                "Amenity": Amenity,
-                "Place": Place,
-                "Review": Review}
-        return classes
 
 
 if __name__ == '__main__':

f5364806a151abda9dccbb9a2768bdde85a40a40
Mwan3kii <agathawanja303@gmail.com>
2024-03-12 20:04:37 +0300

    console commands

--- console.py
+++ console.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
 import cmd
-
+from models import storage
+from models.base_model import BaseModel
 
 class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb) "
@@ -22,6 +23,103 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """Doesnt execute anything when entered"""
         pass
+
+    def do_create(self, line):
+        args = line.split()
+        if not args:
+            print("** class name missing **")
+            return
+        class_name = args[0]
+        if class_name not in storage.all():
+            print("** class doesn't exist **")
+            return
+        object = storage.all()[class_name]()
+        object.save()
+        print(object.id)
+
+    def do_show(self, line):
+        args = line.split()
+        if not args:
+            print("** class name missing **")
+            return
+        if args[0] not in storage.all():
+            print("** class doesn't exist **")
+            return
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+        instance_id = args[1]
+        key = "{}.{}".format(class_name, instance_id)
+        if key not in storage.all():
+            print("** no instance found **")
+            return
+        print(storage.all()[key])
+
+    def do_destroy(self, line):
+        args = line.split()
+        if not args:
+            print("** class name missing **")
+            return
+        class_name = args[0]
+        if class_name not in storage.classes():
+            print("** class doesn't exist **")
+            return
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+        instance_id = args[1]
+        key = "{}.{}".format(class_name, instance_id)
+        if key not in storage.all():
+            print("** no instance found **")
+            return
+        del storage.all()[key]
+        storage.save()
+
+    def do_all(self, line):
+        args = line.split()
+        obj_list = []
+        if not args:
+            for obj in storage.all().values():
+                obj_list.append(str(obj))
+            print(obj_list)
+            return
+        class_name = args[0]
+        if class_name not in [cls.__name__ for cls in storage.all().values()]:
+            print("** class doesn't exist **")
+            return
+        for obj in storage.all().values():
+            if class_name == obj.__class__.__name__:
+                obj_list.append(str(obj))
+        print(obj_list)
+
+    def do_update(self, line):
+        args = line.split()
+        if not args:
+            print("** class name missing **")
+            return
+        class_name = args[0]
+        if class_name not in storage.classes():
+            print("** class doesn't exist **")
+            return
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+        instance_id = args[1]
+        key = "{}.{}".format(class_name, instance_id)
+        if key not in storage.all():
+            print("** no instance found **")
+            return
+        if len(args) < 3:
+            print("** attribute name missing **")
+            return
+        attribute_name = args[2]
+        if len(args) < 4:
+            print("** value missing **")
+            return
+        attribute_value = args[3]
+        obj = storage.all()[key]
+        setattr(obj, attribute_name, attribute_value)
+        storage.save()
 
 
 if __name__ == '__main__':
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -9,26 +9,28 @@ class FileStorage:
     __objects = {}
 
     def all(self):
-        return self.__objects
+        return FileStorage.__objects
 
     def new(self, obj):
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
-        self.__objects[key] = obj
+        FileStorage.__objects[key] = obj
 
     def save(self):
         ser_objs = {}
-        for key, obj in self.__objects.items():
+        for key, obj in FileStorage.__objects.items():
             ser_objs[key] = obj.to_dict()
-        with open(self.__file_path, 'w') as file:
+        with open(FileStorage.__file_path, 'w') as file:
             json.dump(ser_objs, file)
 
     def reload(self):
-        if os.path.exists(self.__file_path):
-            with open(self.__file_path, 'r') as file:
+        try:
+            with open(FileStorage.__file_path, 'r') as file:
                 obj_dict = json.load(file)
                 for key, obj_data in obj_dict.items():
                     class_name, instance_id = key.split('.')
                     cls = globals().get(class_name, None)
                     if cls:
                         obj = cls(**obj_data)
-                        self.__objects[key] = obj
+                        FileStorage.__objects[key] = obj
+        except FileNotFoundError:
+            pass

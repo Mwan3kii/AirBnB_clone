ad162b94d043052ac7a76efe7f1e6b5fa531069a
Mwan3kii <agathawanja303@gmail.com>
2024-03-13 00:44:49 +0300

    update

--- basecheck.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-from models import storage
-class_name = "BaseModel"
-if class_name in storage.all().keys():
-    print("BaseModel exists")
-else:
-    print("BaseModel does not exist")
-
